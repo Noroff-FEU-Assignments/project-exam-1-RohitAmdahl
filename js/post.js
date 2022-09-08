7cff4f27fadf1f5d7df9528a835b9387fb5f6a9d
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-09-09 00:15:17 +0200

    update

--- js/post.js
+++ js/post.js
@@ -1,7 +1,9 @@
 const url = 'https://rohitkumar.rkamdahl.no/wp-json/wp/v2/posts/?per_page=20'
 console.log(url)
 const postContainer = document.getElementById('post-blogs')
+const loading = document.querySelector('.loader')
 console.log(postContainer)
+console.log(loading)
 
 async function blogPost(url) {
   const response = await fetch(url)
@@ -9,19 +11,18 @@ async function blogPost(url) {
   console.log(results)
 
   results.forEach(function (post) {
-    results.innerHTML = `<article class="post-heading">
+    postContainer.innerHTML = `<article class="post-heading">
                         <div class="post-container">
                           <div>
-                            <h1 class="heading">${post.id}</h1>
+                            <h1 class="heading">${post.title.rendered}</h1>
                           </div>
                           <div>
                             <img class="post-image" src="" alt="" />
                           </div>
                           <div>
-                            <p class="project-year"></p>
-                            <p class="description"></p>
+                            <p class="description">${post.content.rendered}</p>
                           </div>
-                        </div>                          
+                        </div>
                     </article> `
   })
 }
