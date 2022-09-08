b423b5c200e0b4bf334cb0b5648a431fce4ea66c
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-09-08 18:36:04 +0200

    update

--- js/post.js
+++ js/post.js
@@ -1,2 +1,16 @@
 const url = 'https://rohitkumar.rkamdahl.no/wp-json/wp/v2/posts/?per_page=20'
 console.log(url)
+const postContainer = document.getElementById('post-blogs')
+console.log(postContainer)
+
+async function blogPost() {
+  try {
+    const response = await fetch(url)
+    const results = await response.json()
+    console.log(results)
+  } catch (error) {
+    console.log(err)
+  }
+}
+
+blogPost()
